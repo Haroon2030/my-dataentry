2a2b7467326b6976f76779b81d582cb9708d32e1
Haroon2030 <142675046+Haroon2030@users.noreply.github.com>
2025-05-26 17:33:24 +0300

    Update settings.py

--- dataentery/settings.py
+++ dataentery/settings.py
@@ -28,7 +28,8 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-ciu25#(k=3wy_p-!#!u4+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='127.0.0.1,localhost,0.0.0.0', cast=Csv())
+ALLOWED_HOSTS = ['my-dataentry.onrender.com', 'localhost', '127.0.0.1']
+
 
 
 # Application definition
